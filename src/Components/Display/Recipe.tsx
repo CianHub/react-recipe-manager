4673203ecd56e69baae01b4f25bcbc090baf1379
Cian O'Gralaigh <ihatecian@gmail.com>
2020-11-01 17:20:36 +0000

    improve destructuring

--- src/Components/Display/Recipe.tsx
+++ src/Components/Display/Recipe.tsx
@@ -5,22 +5,24 @@ interface Props {
   recipe: RecipeType;
 }
 
-const Recipe: React.FC<Props> = ({ recipe }) => {
+const Recipe: React.FC<Props> = ({
+  recipe: { name, servings, instructions },
+}) => {
   return (
     <div>
       <div>
-        <h3>{recipe.name}</h3>
+        <h3>{name}</h3>
         <div>
           <button>Edit</button>
           <button>Delete</button>
         </div>
         <div>
           <span>Servings:</span>
-          <span>{recipe.servings}</span>
+          <span>{servings}</span>
         </div>
         <div>
           <span>Instructions:</span>
-          <div>{recipe.instructions}</div>
+          <div>{instructions}</div>
         </div>
       </div>
     </div>
